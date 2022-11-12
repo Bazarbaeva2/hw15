95ac4ca03a2bd3d27aa476de27ecb57b4019d1df
Dilbar <bdilbar430@gmail.com>
2022-11-13 02:37:32 +0500

    home work 15

---
+++ src/Circle.java
@@ -0,0 +1,31 @@
+import jdk.dynalink.beans.StaticClass;
+
+import java.sql.Statement;
+
+public class Circle {
+    private static double PI;
+    private static double radius;
+
+    public Circle(double PI,double radius) {
+        this.PI=PI;
+        this.radius=radius;
+    }
+    public static void area(){
+        System.out.println("Area of the circle is: "+Math.PI*(radius*radius));
+    }
+
+    public static void circumference(){
+        System.out.println("Circumference of the circle is: "+Math.PI*2*radius);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double  pi = 3.14159;
+        double radius = 15.337;
+        double circumference = (2*pi*radius);
+        double area = (pi*(radius*radius));
+        System.out.println("The radius of the circle " + radius);
+
+                System.out.println("The area of the circle " + area);
+    }
+}
